9f68b2bb4a802e6bae23d5a2d5fe3a3b5f5002b7
sunilgupta2004 <sunil.c@rex.com>
2022-06-29 01:32:33 +0530

    theme switch functionality and header with buttons designed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, Inject} from '@angular/core';
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'finlex-task';
+export class AppComponent implements OnInit{
+  isThemeDark = false;
+
+  constructor(
+    @Inject( DOCUMENT ) private document: Document,
+  ) {
+  }
+
+  ngOnInit() {
+    this.document.body.classList.add('theme-light');
+  }
+
+  toggleTheme(){
+    if(this.isThemeDark){
+      this.document.body.classList.add('theme-light');
+      this.document.body.classList.remove('theme-dark');
+    } else {
+      this.document.body.classList.add('theme-dark');
+      this.document.body.classList.remove('theme-light');
+    }
+    this.isThemeDark = !this.isThemeDark;
+  }
 }
